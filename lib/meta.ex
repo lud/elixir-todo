5e9d062f6d70dfe4d5655b2705a87c1eaa64a975
lud <ludovic@demblans.com>
2022-02-09 18:45:21 +0100

    meta todos

--- lib/meta.ex
+++ lib/meta.ex
@@ -1,4 +1,9 @@
 defmodule TODO.Meta do
   @moduledoc false
   use TODO
+
+  @todo "1.7.0": "prefer macro usage to pass binary VSNs instead of atoms"
+  @todo "1.7.0": "provide a --lines option to show each todo's file:line link"
+  @todo "1.7.0": "provide --next-major --next-minor --next-patch to match the next version"
+  @todo "1.7.0": "ensure the command fails if there are outdated todos"
 end
